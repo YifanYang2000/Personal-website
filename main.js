bc41ebd61875d4c7de6560817a3aecce2f9c91f2
YifanYang2000 <59637035+YifanYang2000@users.noreply.github.com>
2020-02-06 00:58:48 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -6,13 +6,36 @@ window.addEventListener("scroll", function()
 })
 
 
-window.onscroll = function() {myFunction()};
-var nav = document.getElementById("nav");
-var sticky = nav.offsetTop;
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    nav.classList.add("sticky")
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style.top = "0";
   } else {
-    nav.classList.remove("sticky");
+    document.getElementById("nav").style.top = "-82px";
   }
+  prevScrollpos = currentScrollPos;
+}
+
+
+
+var slideIndex = [1,1];
+var slideId = ["slides1", "slides2"]
+showDivs(1, 0);
+showDivs(1, 1);
+
+function plusDivs(n, no) {
+  showDivs(slideIndex[no] += n, no);
+}
+
+function showDivs(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
 }
